c087a354ff7b5f508dab56e1f057a1cf43aaded5
wy471x <wy471x@gmail.com>
2022-09-18 11:51:53 +0800

    cs61b sp2018 hw1 finished.

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 /**
  * @author dunk
  */
-public class ArrayRingBuffer<T> extends AbstractBoundedQueue {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /**
      * Index for the next dequeue or peek.
      */
@@ -40,12 +40,12 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue {
      * @param x
      */
     @Override
-    public void enqueue(Object x) {
+    public void enqueue(T x) {
         if (isFull()) {
             throw new RuntimeException("Ring Buffer Overflow");
         }
 
-        rb[last] = (T) x;
+        rb[last] = x;
         last = (last + 1) % capacity;
         fillCount  += 1;
     }
@@ -67,6 +67,7 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue {
      *
      * @return
      */
+    @Override
     public T dequeue() {
         if (isEmpty()) {
             throw new RuntimeException("Ring Buffer Underflow");
@@ -87,7 +88,7 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue {
     }
 
     @Override
-    public Iterator iterator() {
+    public Iterator<T> iterator() {
         return new ArrayRingBufferIterator();
     }
 
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -2,8 +2,6 @@ package synthesizer;
 import org.junit.Test;
 
 import java.util.Iterator;
-import java.util.Optional;
-
 import static org.junit.Assert.*;
 
 /** Tests the ArrayRingBuffer class.
@@ -11,34 +9,24 @@ import static org.junit.Assert.*;
  */
 
 public class TestArrayRingBuffer {
+    private static final int CAPACITY = 10;
+    private static final int MAX = 10;
+
     @Test
     public void someTest() {
-        ArrayRingBuffer arb = new ArrayRingBuffer(10);
-    }
-
-    @Test
-    public void testOverflow() {
-        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer(10);
-        assertEquals(10, arb.capacity());
-    }
-
-    @Test
-    public void testUnderflow() {
-        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer(10);
-        assertEquals(10, arb.capacity());
+        ArrayRingBuffer arb = new ArrayRingBuffer(CAPACITY);
     }
 
     @Test
     public void testIterator() {
-        ArrayRingBuffer<Integer> arrayRingBuffer = new ArrayRingBuffer<>(10);
-        for (int i = 0; i < 10; i++) {
+        ArrayRingBuffer<Integer> arrayRingBuffer = new ArrayRingBuffer<>(CAPACITY);
+        for (int i = 0; i < MAX; i++) {
             arrayRingBuffer.enqueue(i);
         }
         Iterator<Integer> iterator = arrayRingBuffer.iterator();
         int i = 0;
         while (iterator.hasNext()) {
             int n = iterator.next();
-            System.out.println(n);
             assertEquals("expected:" + i + " actual:" + n, String.valueOf(i), String.valueOf(n));
             i++;
         }

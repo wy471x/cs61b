f91ebc53f6e97034c4b57d729f7abcfb2195f6f6
wy471x <wy471x@gmail.com>
2022-09-21 22:23:14 +0800

    cs61b sp2018 hw2 finishing.

--- sp2018/skeleton-sp18-master/hw2/hw2/Percolation.java
+++ sp2018/skeleton-sp18-master/hw2/hw2/Percolation.java
@@ -3,5 +3,65 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    /**
+     * Create N-by-N grid, with all sites initially blocked.
+     * @param N
+     */
+    public Percolation(int N) {
+
+    }
+
+    /**
+     * Open the site (row, col) if it is not open already.
+     * @param row
+     * @param col
+     */
+    public void open(int row, int col) {
+
+    }
+
+    /**
+     * Is the site (row, col) open?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isOpen(int row, int col) {
+        return true;
+    }
+
+    /**
+     * Is the site (row, col) full?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isFull(int row, int col) {
+        return true;
+    }
+
+    /**
+     * Number of open sites.
+     * @return
+     */
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    /**
+     * Does the system percolate?
+     * @return
+     */
+    public boolean percolates() {
+        return true;
+    }
+
+    /**
+     * Main.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
 
 }
--- sp2018/skeleton-sp18-master/hw2/hw2/PercolationStats.java
+++ sp2018/skeleton-sp18-master/hw2/hw2/PercolationStats.java
@@ -2,4 +2,46 @@ package hw2;
 
 public class PercolationStats {
 
+    /**
+     * Perform T independent experiments on an N-by-N grid.
+     * @param N
+     * @param T
+     * @param pf
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+
+    }
+
+    /**
+     * Sample mean of percolation threshold.
+     * @return
+     */
+    public double mean() {
+        return 0.0;
+    }
+
+    /**
+     * Sample standard deviation of percolation threshold.
+     * @return
+     */
+    public double stddev() {
+        return 0.0;
+    }
+
+    /**
+     * Low endpoint of 95% confidence interval.
+     * @return
+     */
+    public double confidenceLow() {
+        return 0.0;
+    }
+
+    /**
+     * High endpoint of 95% confidence interval.
+     * @return
+     */
+    public double confidenceHight() {
+        return 0.0;
+    }
+
 }

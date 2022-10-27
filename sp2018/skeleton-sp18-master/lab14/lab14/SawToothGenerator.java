5f38103333917418d45f3a92a068f78d1430c175
wy471x <wy471x@gmail.com>
2022-10-27 23:18:56 +0800

    cs61b sp2018 lab14 finishing.

---
+++ sp2018/skeleton-sp18-master/lab14/lab14/SawToothGenerator.java
@@ -0,0 +1,15 @@
+package lab14;
+
+import lab14lib.Generator;
+
+/**
+ * @author dunk
+ * @date 2022/10/27 23:17
+ */
+public class SawToothGenerator implements Generator {
+
+    @Override
+    public double next() {
+        return 0;
+    }
+}

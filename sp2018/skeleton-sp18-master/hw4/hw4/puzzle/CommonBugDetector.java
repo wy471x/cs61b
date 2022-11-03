8fe4df9010e8f46750ba2e06196926059b85e648
wy471x <wy471x@gmail.com>
2022-11-03 22:43:28 +0800

    cs61b sp2018 hw4 finishing.

--- sp2018/skeleton-sp18-master/hw4/hw4/puzzle/CommonBugDetector.java
+++ sp2018/skeleton-sp18-master/hw4/hw4/puzzle/CommonBugDetector.java
@@ -3,6 +3,8 @@ package hw4.puzzle;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by hug. See https://goo.gl/MVL8up for details on these puzzles.
@@ -10,6 +12,7 @@ import java.util.Objects;
 public class CommonBugDetector {
     public static class CommonBugPuzzleState implements WorldState {
         private char name;
+
         public CommonBugPuzzleState() {
             name = 's';
         }
@@ -28,18 +31,39 @@ public class CommonBugDetector {
             }
             return 1;
         }
+//        List.of() supported by java 9
+//        @Override
+//        public Iterable<WorldState> neighbors() {
+//            switch (name) {
+//                case('s'): return createWorldStateList(List.of('a', 'x'));
+//                case('a'): return createWorldStateList(List.of('b'));
+//                case('b'): return createWorldStateList(List.of('c'));
+//                case('c'): return createWorldStateList(List.of('d'));
+//                case('d'): return createWorldStateList(List.of('e'));
+//                case('e'): return createWorldStateList(List.of('g'));
+//                case('x'): return createWorldStateList(List.of('c'));
+//                default: return null;
+//            }
+//        }
 
         @Override
         public Iterable<WorldState> neighbors() {
             switch (name) {
-                case('s'): return createWorldStateList(List.of('a', 'x'));
-                case('a'): return createWorldStateList(List.of('b'));
-                case('b'): return createWorldStateList(List.of('c'));
-                case('c'): return createWorldStateList(List.of('d'));
-                case('d'): return createWorldStateList(List.of('e'));
-                case('e'): return createWorldStateList(List.of('g'));
-                case('x'): return createWorldStateList(List.of('c'));
-                default: return null;
+                case ('s'):
+                    return createWorldStateList(Stream.of('a', 'x').collect(Collectors.toList()));
+                case ('a'):
+                    return createWorldStateList(Stream.of('b').collect(Collectors.toList()));
+                case ('c'):
+                    return createWorldStateList(Stream.of('d').collect(Collectors.toList()));
+                case ('d'):
+                    return createWorldStateList(Stream.of('e').collect(Collectors.toList()));
+                case ('e'):
+                    return createWorldStateList(Stream.of('g').collect(Collectors.toList()));
+                case ('b'):
+                case ('x'):
+                    return createWorldStateList(Stream.of('c').collect(Collectors.toList()));
+                default:
+                    return null;
             }
         }
 
@@ -80,7 +104,7 @@ public class CommonBugDetector {
         AlphabetEasyPuzzle aep = new AlphabetEasyPuzzle('a');
         Solver s3 = new Solver(aep);
         System.out.println("TODO: Print out the number of total things ever"
-                           + " enqueued in your MinPQ and compare to the comments.");
+                + " enqueued in your MinPQ and compare to the comments.");
         // if you print out the total number of items enqueued by s3
         // it should be approximately 25, not approximately 50.
     }
---
+++ sp2018/skeleton-sp18-master/hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,41 @@
+package hw4.puzzle;
+
+/**
+ * @author wanyong
+ * @date 2022/11/3 22:39
+ * @desc
+ **/
+public class Solver {
+
+    /**
+     * Constructor which solves the puzzle, computing
+     * everything necessary for moves() and solution() to
+     * not have to solve the problem again. Solves the
+     * puzzle using the A* algorithm. Assumes a solution exists.
+     *
+     * @param worldState
+     */
+    public Solver(WorldState worldState) {
+
+    }
+
+    /**
+     * Returns the minimum number of moves to solve the puzzle starting
+     * at the initial WorldState.
+     *
+     * @return
+     */
+    public int moves() {
+        return 0;
+    }
+
+    /**
+     * Returns a sequence of WorldStates from the initial WorldState
+     * to the solution.
+     *
+     * @return
+     */
+    public Iterable<WorldState> solution() {
+        return null;
+    }
+}

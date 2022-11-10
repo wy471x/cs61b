5d68dcde65011ffecf5cf04a7facd7d4186179be
wy471x <wy471x@gmail.com>
2022-11-10 12:33:57 +0800

    cs61b sp2018 hw5 finishing.

--- sp2018/skeleton-sp18-master/hw5/SeamCarver.java
+++ sp2018/skeleton-sp18-master/hw5/SeamCarver.java
@@ -5,27 +5,34 @@ import edu.princeton.cs.algs4.Picture;
  * @date 2022/11/9 21:18
  */
 public class SeamCarver {
-    public SeamCarver(Picture picture) {
 
+    private Picture picture;
+
+    public SeamCarver(Picture picture) {
+        this.picture = picture;
     }
 
     // current picture
     public Picture picture()  {
-        return null;
+        return picture;
     }
 
     // width of current picture
     public int width()  {
-        return 0;
+        return picture.width();
     }
 
     // height of current picture
     public int height()   {
-        return 0;
+        return picture.height();
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y)    {
+        return 0.0;
+    }
+
+    private double calcDeltaX() {
         return 0.0;
     }
 

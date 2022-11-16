6c4dc8e75aaed975e20c5dc33b6767f26bb0d521
wy471x <wy471x@gmail.com>
2022-11-16 23:17:47 +0800

    cs61b sp2018 hw7 finishing.

--- sp2018/skeleton-sp18-master/hw7/BinaryTrie.java
+++ sp2018/skeleton-sp18-master/hw7/BinaryTrie.java
@@ -1,4 +1,7 @@
+import edu.princeton.cs.algs4.MinPQ;
+
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -7,15 +10,94 @@ import java.util.Map;
  * @desc
  **/
 public class BinaryTrie implements Serializable {
+
+    private Node root;
+
     public BinaryTrie(Map<Character, Integer> frequencyTable) {
-
+        root = buildTrie(frequencyTable);
     }
 
+    // build the Huffman trie given frequencies
+    private static Node buildTrie(Map<Character, Integer> frequencyTable) {
+        // initialze priority queue with singleton trees
+        MinPQ<Node> pq = new MinPQ<>();
+        for (Map.Entry<Character, Integer> entry : frequencyTable.entrySet()) {
+            if (entry.getValue() > 0) {
+                pq.insert(new Node(entry.getKey(), entry.getValue(), null, null));
+            }
+        }
+
+        // merge two smallest trees
+        while (pq.size() > 1) {
+            BinaryTrie.Node left  = pq.delMin();
+            BinaryTrie.Node right = pq.delMin();
+            BinaryTrie.Node parent = new BinaryTrie.Node('\0', left.freq + right.freq, left, right);
+            pq.insert(parent);
+        }
+        return pq.delMin();
+    }
+
+    // match longest prefix
     public Match longestPrefixMatch(BitSequence querySequence) {
+        Map<Character, BitSequence> characterBitSequenceMap = buildLookupTable();
+        for (Map.Entry<Character, BitSequence> entry : characterBitSequenceMap.entrySet()) {
+            if (checkSequenceEquality(querySequence, entry.getValue())) {
+                return new Match(entry.getValue(), entry.getKey());
+            }
+        }
         return null;
     }
 
+    // check two bit sequence equality
+    private boolean checkSequenceEquality(BitSequence querySequence, BitSequence source) {
+        for (int i = 1; i <= querySequence.length(); i++) {
+            if (querySequence.firstNBits(i).equals(source)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // build lookup table
     public Map<Character, BitSequence> buildLookupTable() {
-        return null;
+        Map<Character, BitSequence> table = new HashMap<>();
+        buildCode(table, root, new BitSequence());
+        return table;
+    }
+
+    // make a lookup table from symbols and their encodings
+    private static void buildCode(Map<Character, BitSequence> table, Node x, BitSequence s) {
+        if (!x.isLeaf()) {
+            buildCode(table, x.left, s.appended(0));
+            buildCode(table, x.right, s.appended(1));
+        } else {
+            table.put(x.ch, s);
+        }
+    }
+
+    // Huffman trie node
+    private static class Node implements Comparable<BinaryTrie.Node>, Serializable {
+        private final char ch;
+        private final int freq;
+        private final BinaryTrie.Node left, right;
+
+        Node(char ch, int freq, BinaryTrie.Node left, BinaryTrie.Node right) {
+            this.ch    = ch;
+            this.freq  = freq;
+            this.left  = left;
+            this.right = right;
+        }
+
+        // is the node a leaf node?
+        private boolean isLeaf() {
+            assert ((left == null) && (right == null)) || ((left != null) && (right != null));
+            return (left == null) && (right == null);
+        }
+
+        // compare, based on frequency
+        @Override
+        public int compareTo(BinaryTrie.Node that) {
+            return this.freq - that.freq;
+        }
     }
 }
--- sp2018/skeleton-sp18-master/hw7/HuffmanEncoder.java
+++ sp2018/skeleton-sp18-master/hw7/HuffmanEncoder.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -6,11 +9,36 @@ import java.util.Map;
  * @desc
  **/
 public class HuffmanEncoder {
+
+    private static final String FILE_PATH = "watermelonsugar.txt";
+    private static final String FILE_SUFFIX = ".huf";
+
     public static Map<Character, Integer> buildFrequencyTable(char[] inputSymbols) {
-        return null;
+        Map<Character, Integer> table = new HashMap<>();
+        for (int i = 0; i < inputSymbols.length; i++) {
+            table.put(inputSymbols[i], table.getOrDefault(inputSymbols[i], 0) + 1);
+        }
+        return table;
     }
 
     public static void main(String[] args) {
-
+        if (args.length < 1) {
+            throw new IllegalArgumentException("number of arguments less than 1.");
+        }
+        char[] chars = FileUtils.readFile(FILE_PATH);
+        Map<Character, Integer> characterIntegerMap = buildFrequencyTable(chars);
+        BinaryTrie binaryTrie = new BinaryTrie(characterIntegerMap);
+        ObjectWriter objectWriter = new ObjectWriter(args[0] + FILE_SUFFIX);
+        objectWriter.writeObject(binaryTrie);
+        objectWriter.writeObject(chars.length);
+        Map<Character, BitSequence> characterBitSequenceMap = binaryTrie.buildLookupTable();
+        List<BitSequence> bitSequenceArrayList = new ArrayList<BitSequence>();
+        for (int i = 0; i < chars.length; i++) {
+            if (characterBitSequenceMap.containsKey(chars[i])) {
+                bitSequenceArrayList.add(characterBitSequenceMap.get(chars[i]));
+            }
+        }
+        BitSequence assemble = BitSequence.assemble(bitSequenceArrayList);
+        objectWriter.writeObject(assemble);
     }
 }
